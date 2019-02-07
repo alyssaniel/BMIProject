579f67ca824d2c98640352e9b60e823109754947
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-02-07 08:16:36 -0500

    Made the wrong Coding

---
+++ src/com/Lickingheights/Main.java
@@ -0,0 +1,44 @@
+package com.Lickingheights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String userName;
+        Scanner keyboard;
+        int userFeet, userInches, userPounds;
+
+
+        //initializing
+        keyboard= new Scanner(System.in);
+        total =
+
+        System.out.println("What is your name?");
+        userName=keyboard.nextLine();
+
+        System.out.println("What is your height in only feet?");
+        userFeet=keyboard.nextLine();
+
+        System.out.println("What is your height in only inches?");
+        userInches=keyboard.nextLine();
+
+        System.out.println("How much do you weigh in pounds?");
+        userPounds=keyboard.nextLine();
+
+
+        System.out.println("");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
